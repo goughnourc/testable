641bdea984985e5df1ae51e99bc31ee638e0665f
Chris Goughnour <goughnourc@gmail.com>
2018-04-04 07:19:51 -0700

    Change test_strategy implication constructor and update proofs accordingly.

--- testable.v
+++ testable.v
@@ -7,6 +7,7 @@ Module testable.
 (* Everything seems to work fine without including testable as parameter to test_strategy
    and its inclusion makes ts_mp harder to use. *)
 (* It may still be useful--maybe for automating the construction of test strategies. *)
+(* TODO: Move "testable" definition to its own module--or move everything else. *)
 Inductive testable : Prop -> Type :=
 | t_prim : forall P, testable P
 | t_not : forall P, testable P -> testable (~P)
@@ -46,11 +47,16 @@ match l with
 | xs :: l' => xs ++ (flatten l')
 end.
 
+(* Proof irrelevance of P allows us to test P -> Q by testing Q once in the case that P is inhabited. *)
+(* Unfortunately, we need to know whether or not we'll be testing Q in order to generate a list of tests
+   to run (to_test), so it looks like we can't use this approach. *)
+Definition PI (P : Prop) := forall p1 p2 : P, p1 = p2.
+
 Section test_strategy.
   Variable test : Type.
   Variable prop_tested : test -> Prop.
 
-  (* It seems that we don't want test strategies that require running an infinite number of tests *)
+  (* We don't want test strategies that require running an infinite number of tests. *)
   Definition has_tests := bool.
 
   (* TODO: Improve implicit and explicit arguments. It may be necessary to change the following constructors. *)
@@ -64,16 +70,19 @@ Section test_strategy.
   | ts_exists : forall ht, forall A, forall P : (A -> Prop), forall x : A,
     test_strategy ht (P x) -> test_strategy ht (exists a : A, P a)
     (* Don't allow testing every element of A unless we know A is finite. *)
+    (* TODO: just delete this constructor? *)
   | ts_all : forall A, forall P : (A -> Prop),
     (forall a : A, test_strategy false (P a)) -> test_strategy false (forall a : A, P a)
     (* Assume that we went to the trouble of showing that A is finite so that we could add tests. *)
+    (* TODO: Calculate whether or not any tests were actually used. *) 
   | ts_all_fin : forall A, forall lA : listable A, forall P : (A -> Prop),
     forall ht : (A -> has_tests),
     (forall a : A, test_strategy (ht a) (P a)) -> test_strategy true (forall a : A, P a)
-    (* For now, don't allow testing for every witness of P. *)
-    (* TODO: Revisit this. *)
-  | ts_impl : forall P Q : Prop,
-    (forall p : P, test_strategy false Q) -> test_strategy false (P -> Q)
+    (* Check that proving P is unnecessary and remove it. *)
+  | ts_impl_and : forall ht, forall P Q : Prop,
+    test_strategy ht (P /\ Q) -> test_strategy ht (P -> Q)
+  | ts_impl_not : forall ht, forall P Q : Prop,
+    test_strategy ht (~P) -> test_strategy ht (P -> Q)
   | ts_mp : forall htPimpQ htP, forall P Q : Prop,
     test_strategy htPimpQ (P -> Q) -> test_strategy htP P -> test_strategy (orb htPimpQ htP) Q.
 
@@ -88,7 +97,8 @@ Section test_strategy.
   | ts_exists _ A _ _ tsPx => exists a : A, (proven tsPx)
   | ts_all A _ tsP' => forall a : A, (proven (tsP' a))
   | ts_all_fin A _ _ _ tsP' => forall a : A, (proven (tsP' a))
-  | ts_impl P' _ tsQ => forall p : P', (proven (tsQ p))
+  | ts_impl_and _ _ _ ts => proven ts
+  | ts_impl_not _ _ _ ts => proven ts
   | ts_mp _ _ _ _ tsPimpQ tsP => (proven tsPimpQ) /\ (proven tsP)
   end.
 
@@ -103,7 +113,8 @@ Section test_strategy.
   | ts_exists _ A _ _ tsPx => exists a : A, (assumed tsPx)
   | ts_all A _ tsP' => forall a : A, (assumed (tsP' a))
   | ts_all_fin A _ _ _ tsP' => forall a : A, (assumed (tsP' a))
-  | ts_impl P' _ tsQ => forall p : P', (assumed (tsQ p))
+  | ts_impl_and _ _ _ ts => assumed ts
+  | ts_impl_not _ _ _ ts => assumed ts
   | ts_mp _ _ _ _ tsPimpQ tsP => (assumed tsPimpQ) /\ (assumed tsP)
   end.
 
@@ -123,27 +134,36 @@ Section test_strategy.
       + apply IHtsP1. assumption.
       + apply IHtsP2. assumption.
     - intros Hexists. destruct Hexists as [ _ Hassumed ]. exists x. apply IHtsP. apply Hassumed.
-    - intros Hall. intros HP. apply (H HP). apply (Hall HP).
+    - intros Hassumed HP. assert (P /\ Q) as Hand.
+      + apply IHtsP. apply Hassumed.
+      + destruct Hand as [ _ HQ ]. apply HQ.
+    - intros Hassumed HP. assert (~P) as HNP.
+      + apply IHtsP. apply Hassumed.
+      + exfalso. apply HNP. apply HP.
     - intros Hand. destruct Hand as [ HAtsP1 HAtsP2 ]. assert (P -> Q) as HPimpQ.
       + apply IHtsP1. apply HAtsP1.
       + apply HPimpQ. apply IHtsP2. apply HAtsP2.
   Qed.
 
   (* Given a strategy, create a list of the tests that must be run. *)
+  (* TODO: This could be made more efficient by using the value of ht. *)
   Fixpoint to_test {ht : has_tests} {P : Prop} (tsP : test_strategy ht P) : list test :=
   match tsP with
   | ts_prove _ _ => nil
-  | ts_test t =>  t :: nil
+  | ts_test t => t :: nil
   | ts_or_l _ _ _ tsP => to_test tsP
   | ts_or_r _ _ _ tsQ => to_test tsQ
   | ts_and _ _ _ _ tsP tsQ => (to_test tsP) ++ (to_test tsQ)
   | ts_exists _ A _ _ tsPx => to_test tsPx
   | ts_all A _ tsP' => nil (* Tests are disallowed in this case. *)
   | ts_all_fin A lA _ _ tsP' => flatten (map (fun a => to_test (tsP' a)) (listable_to_list lA))
-  | ts_impl P' _ tsQ => nil (* Tests are disallowed in this case, too. *)
+  | ts_impl_and _ _ _ ts => to_test ts
+  | ts_impl_not _ _ _ ts => to_test ts
   | ts_mp _ _ _ _ tsPimpQ tsP => (to_test tsPimpQ) ++ (to_test tsP)
   end.
 
+  (* TODO: Show that if every proposition to test holds then assumed holds. *)  
+  (* Definition coverage_complete ... *)
 End test_strategy.
 Print test_strategy.
 Arguments test_strategy {test} {prop_tested} ht P.
@@ -162,8 +182,8 @@ Definition ex1_pt : ex1_test -> Prop.
 Defined.
 
 Definition ts_ex1 : (@test_strategy ex1_test ex1_pt false example_1).
-  unfold example_1. apply ts_all. intros P. apply ts_impl.
-  intros HP. apply ts_prove. apply HP.
+  unfold example_1. apply ts_prove. 
+  auto.
 Defined.
 
 (*
@@ -171,20 +191,15 @@ Compute proven ts_ex1.
 (* forall x : Prop, x -> x *)
 
 Compute assumed ts_ex1.
-(* forall x : Prop, x -> True *)
+(* True *)
 
 Compute to_test ts_ex1.
 (* nil *)
 *)
 
-(* We don't need any hypotheses to prove (assumed ts_ex1) *)
-Theorem ts_ex1_assumed_trivial : assumed ts_ex1.
-  simpl. intros _ _. constructor.
-Qed.
-
 Theorem ts_ex1_correct : example_1.
   apply (test_strategy_correct ts_ex1).
-  apply ts_ex1_assumed_trivial.
+  constructor.
 Qed.
 
 Definition example_2 := forall P Q : Prop, P -> Q /\ P.
